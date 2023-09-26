81fe2526554dcf8369dc2c91538e0f8e313fcab4
CoinFLip2 <pierrelouis3131@gmail.com>
2023-09-26 11:08:55 +0200

    ajout du vrai texte + le droit d'éxécution

--- Job01/myfirstscript.sh
+++ Job01/myfirstscript.sh
@@ -1,1 +1,9 @@
 echo "my first script"
+
+#pour créer un répertoire (ne pas oublier d'utiliser la commande dans le fichier souhaiter): git clone "lien du répertoire" 
+#pour créer un sous-dossier (ne pas oublier d'être au bon endroit): mkdir "nom du sous dossier"
+#pour créer un fichier ne pas oublier de spécifier le type de doc grâce à '.txt' '.sh' '.exe' ect: vim "nom_du_fichier.exe"
+#pour vim , appuyer sur 'echap' pour sotir du mode écriture et 'i' pour y revenir, appuyer sur ':' et écrire 'qw' pour sauvegarder et quitter
+#pour donner les droit d'execution: chmod +x "mon_script.sh"
+# et pour l'éxécuter : ./"mon_script.sh"
+
