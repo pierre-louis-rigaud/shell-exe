9decd391139334e26a41b9f32b2c952c8be67aa6
CoinFLip2 <pierrelouis3131@gmail.com>
2023-09-28 12:15:54 +0200

    job08 refonte

--- Job08/get_logs.sh
+++ Job08/get_logs.sh
@@ -1,24 +1,30 @@
-#!/bin/bash
+#!/usr/bin/bash
+
+destination_dir="Backup"
+
+cd "$destination_dir" || exit 1
 
 #répertoire de sauvegarde
 backup_dir="Backup"
 
 #Crée le répertoire de sauvegard s'il n'existe pas
-#mkdir -p "$backup_dir"
+mkdir -p "$backup_dir"
 
-#Otient la date et l'heure
-date_format=$(date +'%d-%m-%Y-%H:%M')
-
-#nom du fichier de sortie
-output_file="number_connection-$date_format.log"
 
 #commande qui extrait le nombre de connexions
-number_connections=$(who | wc -l)
+number_connections=$(who | grep "$(whoami)" | wc -l)
+
+#Otient la date et l'heure
+fichier_de_sortie="$backup_dir/number_connection-$(date +'%d-%m-%Y-%H:%M').tar"
+
 
 #Crée le fichier de sortie avec le nbr de connections
-echo "Nombre de connexions à la VM : $number_connections" > "$output_file"
+echo "Nombre de connexions à la VM : $number_connections" > "$fichier_de_sortie" 
 
-#Archive le fichier de sortie dans un fichier tar.gz
-tar -czvf "$backup_dir/$output_file.tar.gz" "$output_file"
+#Archive le fichier de sortie dans un fichier tar
+tar -cvf "$fichier_de_sortie"  "$fichier_de_sortie" 
+
+#supprime le fichier numbers
+rm "$fichier_de_sortie" 
 
 
