72d7451fa934f4cd8a27dea9f58e7f10b3dc9bf4
pierre-louis-rigaud <pierre-louis.rigaud@laplateforme.io>
2023-10-03 17:01:26 +0200

    refinement

--- Job05/hello_bye.sh
+++ Job05/hello_bye.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-#récupère l'argument 
-argument="$1"
-
 #vérifie la valeur de l'argument et affiche le bon message
-if [ "$argument" = "Hello" ]; then
+if [ "$1" = "Hello" ]; then
 	echo "Bonjour, je suis un script !"
-elif [ "$argument" = "Bye" ]; then
+elif [ "$1" = "Bye" ]; then
 	echo "Au revoir et bonne journée !"
-elif [ "$argument" = "Surprise" ]; then 
+elif [ "$1" = "Surprise" ]; then 
 	echo "Joyeux Anniversaire!!!"
 else
 	echo "Argument non reconnu. Utilisez 'Hello' ou 'Bye'."
